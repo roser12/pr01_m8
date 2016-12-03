ad9679604f7b6c153ecc11c9269c9bd30b46c99b
roser Queralta <roserqueralta@yahoo.es>
2016-12-03 17:14:55 +0100

    "Versio 0.1 projecte pr01_m8

--- pr01_m8/src/pr01_m8/Pr01_m8.java
+++ pr01_m8/src/pr01_m8/Pr01_m8.java
@@ -1,7 +1,12 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package pr01_m8;
 
-import java.io.*; 
+import java.io.*;
+       
 public class Pr01_m8 {
 
    
@@ -16,6 +21,7 @@ public class Pr01_m8 {
 public static void main(String[] args) throws IOException {         
 File f = new File("fitxer.html");         
 try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) { 
+
 bw.write("<html>");bw.newLine(); 
 bw.write("  <head>");bw.newLine();
 bw.write("    <title>");bw.newLine();
@@ -38,6 +44,11 @@ bw.close();
 }
 } 
 } 
+        // TODO code application logic here
+    
+    
+
+
         // TODO code application logic here
     
     
