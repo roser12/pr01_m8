3a103e5b2d5a5f02863c6e78a6c9d87671912d9f
roser Queralta <roserqueralta@yahoo.es>
2016-12-03 17:23:49 +0100

    "Versio 0.2 projecte pr01_m8

--- pr01_m8/src/pr01_m8/Pr01_m8.java
+++ pr01_m8/src/pr01_m8/Pr01_m8.java
@@ -21,6 +21,8 @@ public class Pr01_m8 {
 public static void main(String[] args) throws IOException {         
 File f = new File("fitxer.html");         
 try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) { 
+ bw.write(" <h1>DAW2 m08uf4pr2</h1>");
+ bw.newLine(); 
 
 bw.write("<html>");bw.newLine(); 
 bw.write("  <head>");bw.newLine();
